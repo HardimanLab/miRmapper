feea3ccd6a72c910aab0993d2650e5ea26a2179c
glen <glen@musc.edu>
2017-09-14 14:24:25 -0400

    working backwords

--- R/initialize.R
+++ R/initialize.R
@@ -3,11 +3,11 @@ setMethod('initialize', 'miRmapper',
           {
             # Remove 'NA' values
             # Doesn't seem to work with lists
-            #interactions <- interactions[complete.cases(interactions), ]
-            logicalMatrix = !sapply(Int, is.na)
-            logicalVector = as.logical(logicalMatrix[,1] * logicalMatrix[,2])
-            interactions = data.frame(interactions[1][logicalVector], 
-                interactions[2][logicalVector])
+            interactions <- interactions[complete.cases(interactions), ]
+            #logicalMatrix = !sapply(Int, is.na)
+            #logicalVector = as.logical(logicalMatrix[,1] * logicalMatrix[,2])
+            #interactions = data.frame(interactions[1][logicalVector], 
+            #    interactions[2][logicalVector])
             DEgenes <- DEgenes[!is.na(DEgenes)]
             DEgenes <- data.frame(DEgenes)
             .Object@interactions = interactions
