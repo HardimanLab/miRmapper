384c593df0e450db4ae427f533ac82bba552a754
glen <glen@musc.edu>
2017-09-14 12:58:38 -0400

    fixing NA list problem with interactions

--- R/initialize.R
+++ R/initialize.R
@@ -2,7 +2,10 @@ setMethod('initialize', 'miRmapper',
           function(.Object, interactions, DEgenes)
           {
             # Remove 'NA' values
-            interactions <- interactions[complete.cases(interactions), ]
+            # Doesn't seem to work with lists
+            #interactions <- interactions[complete.cases(interactions), ]
+            interactions = list(interactions[[1]][complete.cases(interactions)],
+                interactions[[2]][complete.cases(interactions)])
             DEgenes <- DEgenes[!is.na(DEgenes)]
             DEgenes <- data.frame(DEgenes)
             .Object@interactions = interactions
