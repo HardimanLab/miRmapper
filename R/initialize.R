2e907286c9f7872077dbe20df65cdcbb6d04db51
glen <glen@musc.edu>
2017-09-14 14:37:22 -0400

    Update initialize.R

--- R/initialize.R
+++ R/initialize.R
@@ -2,12 +2,7 @@ setMethod('initialize', 'miRmapper',
           function(.Object, interactions, DEgenes)
           {
             # Remove 'NA' values
-            # Doesn't seem to work with lists
             interactions <- interactions[complete.cases(interactions), ]
-            #logicalMatrix = !sapply(Int, is.na)
-            #logicalVector = as.logical(logicalMatrix[,1] * logicalMatrix[,2])
-            #interactions = data.frame(interactions[1][logicalVector], 
-            #    interactions[2][logicalVector])
             DEgenes <- DEgenes[!is.na(DEgenes)]
             DEgenes <- data.frame(DEgenes)
             .Object@interactions = interactions
